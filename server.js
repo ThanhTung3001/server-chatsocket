95959443a7ed8409202b134b594355424927e139
Lê Viết Thanh Tùng <thanhtung@MacBook-Pro-Cua-Tung.local>
2022-09-03 20:59:10 +0700

    commit

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const socketIo = require("socket.io")(server, {
   }); 
   // nhớ thêm cái cors này để tránh bị Exception nhé :D  ở đây mình làm nhanh nên cho phép tất cả các trang đều cors được. 
 
-
+app.get('/',(req,res)=>{
+    res.sendFile( __dirname+"/index.html")
+})
 socketIo.on("connection", (socket) => { ///Handle khi có connect từ client tới
   console.log("New client connected" + socket.id); 
    socket.emit('getId',socket.id);
